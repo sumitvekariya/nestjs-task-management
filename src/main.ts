98ee23378a6e7b0be4e101e78d08cac2563d17a6
Sumit <sumitvekariya7@gmail.com>
2020-08-20 10:13:40 +0530

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // if () {
-  app.enableCors({ origin: 'http://localhost:3001' })
+  app.enableCors();
   // }
 
   const port = process.env.port || serverConfig.port;
